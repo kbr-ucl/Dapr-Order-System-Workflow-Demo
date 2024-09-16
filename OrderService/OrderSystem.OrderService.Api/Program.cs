5fbc247597f0ce5c9afbbdd3c4c92f217e049163
Kaj Bromose <kbr@ucl.dk>
2024-09-16 18:33:26 +0200

    Virker lidt nu :-) Der er hul fra API gateway via sidecar til service

--- OrderService/OrderSystem.OrderService.Api/Program.cs
+++ OrderService/OrderSystem.OrderService.Api/Program.cs
@@ -1,5 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
 
+// Configure the services
+// Add services to the container.
+//var daprHttpPort = DaprHttpPort();
+//var daprGrpcPort = DaprGrpcPort();
+//builder.Services.AddDaprClient(config => config
+//    .UseHttpEndpoint($"http://localhost:{daprHttpPort}")
+//    .UseGrpcEndpoint($"http://localhost:{daprGrpcPort}"));
+
+//var daprHttpPort = Environment.GetEnvironmentVariable("DAPR_HTTP_PORT") ?? "32000";
+var daprGrpcPort = Environment.GetEnvironmentVariable("DAPR_GRPC_PORT") ?? "3800";
+builder.Services.AddDaprClient(config => config
+    //.UseHttpEndpoint($"http://localhost:{daprHttpPort}")
+    .UseGrpcEndpoint($"http://localhost:{daprGrpcPort}"));
+//builder.Services.AddDaprClient();
+
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -20,3 +35,22 @@ app.MapGet("/hello", () => "Order: Hello World!").WithOpenApi();
 
 app.Run();
 
+
+string DaprHttpPort()
+{
+    var result = Environment.GetEnvironmentVariable("DAPR_HTTP_PORT");
+    if (!string.IsNullOrEmpty(result)) return result;
+
+    result = Environment.GetEnvironmentVariable("ASPNETCORE_HTTP_PORTS");
+    if (!string.IsNullOrEmpty(result)) return result;
+
+    return "8080";
+}
+
+string DaprGrpcPort()
+{
+    var result = Environment.GetEnvironmentVariable("DAPR_GRPC_PORT");
+    if (!string.IsNullOrEmpty(result)) return result;
+    
+    return "3800";
+}
